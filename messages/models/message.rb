35f68fd2d7708f85f956c9473dbc535e8c5d7e58
aviflombaum <avi@flombaum.com>
2013-10-21 09:27:10 -0400

    back

---
+++ messages/models/message.rb
@@ -0,0 +1,11 @@
+class Message
+  include DataMapper::Resource
+
+  puts self
+
+  property :id, Serial            # Auto-increment integer id
+  property :from, String          # A short string of text
+  property :content, Text         # A longer text block
+  property :created_at, DateTime  # Auto assigns data/time
+
+end
